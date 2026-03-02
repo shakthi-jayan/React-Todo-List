f7d29b2524fd90ea57d03790015abb737464241f
Shakthi Jayan J <shakthijayan19@gmail.com>
2026-03-02 20:34:59 +0530

    Initial backend setup

---
+++ backend/server.js
@@ -0,0 +1,6 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+const app = express()
+const port = process.env.PORT
+app.listen(port,()=>{ console.log(`Server is running on : https://localhost:${port}`)})

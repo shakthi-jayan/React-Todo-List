e95be2369f30bb40ca208ad2c236c9f54ad76031
Shakthi Jayan J <shakthijayan19@gmail.com>
2026-03-02 20:47:38 +0530

    Mongoose Installation and MongoDB Atlas Connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
+import mongoose from 'mongoose'
 const app = express()
 const port = process.env.PORT
-app.listen(port,()=>{ console.log(`Server is running on : https://localhost:${port}`)})
+const mongouri = process.env.MONGO_URI
+mongoose.connect(mongouri)
+.then(
+    app.listen(port,()=>{
+        console.log("Mongodb Successfully connected")
+        console.log("Server is running")
+    })
+).catch(err => console.log(err))
+

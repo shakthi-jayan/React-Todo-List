b38b298d32a76b532f72a48969e54edde3bf5393
Shakthi Jayan J <shakthijayan19@gmail.com>
2026-03-09 20:25:46 +0530

    Added Routes for Editing Tasks

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,6 +1,12 @@
 import express from 'express'
-import { addTask, getTasks } from '../controllers/taskController.js'
+import { addTask, getTasks, getTask, updateTask, deleteTask } from '../controllers/taskController.js'
+
 const router = express.Router()
-router.get('/',getTasks)
-router.post('/add',addTask)
+
+router.get('/', getTasks)
+router.get('/:id', getTask)
+router.post('/add', addTask)
+router.put('/:id', updateTask)
+router.delete('/:id', deleteTask)
+
 export default router

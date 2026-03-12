7d76236ca2dfed756c08a7cf056c758dbbfdb191
Shakthi Jayan J <shakthijayan19@gmail.com>
2026-03-12 20:18:18 +0530

    Cors connected for frontend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
+import cors from 'cors'
 import mongoose from 'mongoose'
 import router from './routes/taskRoutes.js'
 import userRouter from './routes/userRoutes.js'
 const app = express()
 app.use(express.json())
+app.use(cors({
+  origin: "http://localhost:5173",  // frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use('/api/backend',router)
 app.use('/api/users',userRouter)
 const port = process.env.PORT
